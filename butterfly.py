cd6c8b88b42ff57f6b96c34bdd08936d2591a15e
logickiddie <72816417+logickiddie@users.noreply.github.com>
2022-10-01 02:40:02 +0530

    Update butterfly.py

--- butterfly.py
+++ butterfly.py
@@ -9,18 +9,18 @@ print("Generated butterfly pattern is:\n")
 for i in range(1,n+1):
     for j in range(1, 2*n+1):
         if j>i and j< 2*n+1-i:
-            print("  ", end="")
+            print("   ", end="")
         else:
-            print("* ", end="")
+            print("*  ", end="")
     print()
 
 # Lower part
 for i in range(n,0,-1):
     for j in range(2*n,0,-1):
         if j>i and j< 2*n+1-i:
-            print("  ", end="")
+            print("   ", end="")
         else:
-            print("* ", end="")
+            print("*  ", end="")
     print()   
     
     #END OF PROGRAM
